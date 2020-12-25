b0b1fb6cbc2fbdd5053228f51a96fa2166237f85
yuyiba <yushuai027@163.com>
2020-12-25 17:34:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from PIL import Image
 import os.path
 import glob
 from pylab import *
+from tqdm import tqdm
 
-
+if not os.path.exists("checkpoints"):
+        os.makedirs("checkpoints")
+if not os.path.exists("result"):
+        os.makedirs("result")
 # 是否使用cuda
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -31,7 +35,7 @@ parse=argparse.ArgumentParser()
 def train_model(model, criterion, optimizer, dataload, num_epochs=100):
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        print('-' * 40)
         dt_size = len(dataload.dataset)
         epoch_loss = 0
         step = 0
@@ -61,7 +65,8 @@ def train_model(model, criterion, optimizer, dataload, num_epochs=100):
     return model
 
 #losses_his=[]
-writer = SummaryWriter('/home/yus/Documents/u_net_liver-master/tensorboard/6.14road/bs20_unet_GoOn')
+
+writer = SummaryWriter('/home/yus/Documents/Unet-SE-master/log/')
 
 
 #训练模型
@@ -79,21 +84,19 @@ def train():
 
 #显示模型的输出结果
 def test():
-    model = Unet(3, 1)
-    model.load_state_dict(torch.load(args.ckp,map_location='cpu'))
-    liver_dataset = LiverDataset("data/val", transform=x_transforms,target_transform=y_transforms)
-
+    model = Unet(3, 1).to(device)
+    model.load_state_dict(torch.load(args.ckp))
+    liver_dataset = LiverDataset("data/test", transform=x_transforms,target_transform=y_transforms)
     dataloaders = DataLoader(liver_dataset, batch_size=1)
     model.eval()
-    import matplotlib.pyplot as plt
-    plt.ion()
     with torch.no_grad():
-        for x, _ ,x_path in dataloaders:
+        for x, _ ,x_path in tqdm(dataloaders):
             x_path=str(x_path).split("/")
-            print(x_path[2][:-3])
+            x = x.to(device)
             y=model(x)
             img_numpy = y[0].cpu().float().numpy()
-            img_numpy = (np.transpose(img_numpy, (1, 2, 0))) / 1.0 * 255.0
+            img_numpy = (np.transpose(img_numpy, (1, 2, 0)))
+            img_numpy = (img_numpy >= 0.5) * 255
             img_out=img_numpy.astype(np.uint8)
             imgs = transforms.ToPILImage()(img_out)
             imgs.save('result/'+x_path[2][:-3])
@@ -104,7 +107,7 @@ def test():
 if __name__ == '__main__':
     parse = argparse.ArgumentParser()
     parse.add_argument("action", type=str, help="train or test")
-    parse.add_argument("--batch_size", type=int, default=20) #6
+    parse.add_argument("--batch_size", type=int, default=4) #6
     parse.add_argument("--ckp", type=str, help="the path of model weight file")
     args = parse.parse_args()
 
